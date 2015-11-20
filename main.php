1f8b531e9b94839d152d1dc5c1a49b11fac3114f
Donovan Tengblad <purplefish32@gmail.com>
2015-11-20 14:14:27 +0100

    Initial commit

---
+++ main.php
@@ -0,0 +1,30 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/src/Pusher.php';
+
+use Innova\Pusher as Pusher;
+
+$host = "http://localhost";
+$port = "8001";
+
+$app = "1278690433";
+$key = "98f0b777783fe1bca391";
+$secret = "none";
+
+$channels = array(
+	'/messages'
+);
+
+$event = "my-event";
+
+$data = array(
+	'title' => "This is the title",
+	'body' => "This is the body!"
+);
+
+//$channels = array('messages');
+
+$pusher = new Pusher($host, $port, $app, $key, $secret);
+
+$pusher->trigger($channels, $event, $data);
---
+++ src/Pusher.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Innova;
+
+class Pusher
+{
+	
+	protected $host;
+	protected $port;
+	protected $app;
+	protected $key;
+	protected $timeout;
+	protected $url;
+
+	public function __construct($host, $port, $app, $key, $secret, $timeout = null) {
+		$this->host = $host;
+		$this->port = $port;
+		$this->app  = $app;
+		$this->key  = $key;
+
+		if($port !== null) {
+			$this->url = $this->host . ":" . $this->port;
+		} else {
+			$this->url = $this->host;
+		}
+	}
+
+	public function trigger($channels, $event, $data, $alreadyEncoded = false) {
+
+		if(is_string($channels) === true ) {
+			$channels = array($channels);
+		}
+
+		$this->validate_channels($channels);
+
+		$dataEncoded = $alreadyEncoded ? $data : json_encode($data);
+
+		print_r($dataEncoded);
+
+		$client = new \GuzzleHttp\Client();
+
+		# TODO check channels array etc...
+
+		# TODO multi channel in one post ?
+		foreach ($channels as $channel) {
+			$response = $client->request('POST', $this->url, [
+			    'json' => [
+			        'channel' => '/' . $this->app . $channel,
+			        'data' => [
+			        	'app' => $this->app,
+			        	'key' => $this->key,
+			        	'event' => $event,
+			        	'data' => $dataEncoded
+			        ],
+			    ]
+		    ]);
+		}
+		
+		if ($response->getStatusCode() == 200)
+		{
+			echo 'OK';
+			return true;
+		} 
+		else 
+		{
+			echo 'NOK';
+			return false;
+		}
+
+	}
+
+	public function getChannels() {
+
+	}
+
+	/**
+	 * validate number of channels and channel name format.
+	 */
+	private function validate_channels($channels) 
+	{
+		if( count( $channels ) > 100 ) {
+			throw new PusherException('An event can be triggered on a maximum of 100 channels in a single call.');
+		}
+		foreach ($channels as $channel) {
+			$this->validate_channel($channel);
+		}
+	}
+
+	/**
+	 * Ensure a channel name is valid based on our spec
+	 */
+	private function validate_channel($channel)
+	{
+		/*if (!preg_match('/\A[-a-zA-Z0-9_=@,.;]+\z/', $channel)) {
+			throw new PusherException('Invalid channel name ' . $channel);
+		}*/
+	}
+}
+
+class PusherException extends \Exception
+{
+}
